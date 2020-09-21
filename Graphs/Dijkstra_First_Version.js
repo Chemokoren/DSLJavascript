28744162299fbf34f5cdfe4917ae5911c5a05415
Kibet Masai <humphreykibet@gmail.com>
2020-09-21 09:41:53 +0300

    updated Binary heaps hkm

--- Graphs/Dijkstra_First_Version.js
+++ Graphs/Dijkstra_First_Version.js
@@ -106,4 +106,4 @@ graph.addEdge("C", "F", 4);
 graph.addEdge("D", "E", 3);
 graph.addEdge("D", "F", 1);
 graph.addEdge("E", "F", 1);
-console.log(graph.Dijkstra("A","B"))
+console.log(graph.Dijkstra("A","F"))
