bafa2b2b5bfac12afd3bc3306a0857283a32d905
Kibet Masai <humphreykibet@gmail.com>
2020-10-08 07:00:58 +0300

    Sorting/Sort_Quick.js hkm

--- Sorting/Sort_Quick.js
+++ Sorting/Sort_Quick.js
@@ -1,12 +1,13 @@
+function swap1(array, i, j){
+    var temp =array[i];
+    array[i] = array[j]
+    array[j] =temp;
+}
+const swap =(arr,idx1, idx2)=>{
+    [arr[idx1],arr[idx2] =[arr[idx2],arr[idx1]]];
+};
+
 function pivot(arr, start=0, end=arr.length+1){
-    // function swap(array, i, j){
-    //     var temp =array[i];
-    //     array[i] = array[j]
-    //     array[j] =temp;
-    // }
-    const swap =(arr,idx1, idx2)=>{
-        [arr[idx1],arr[idx2] =[arr[idx2],arr[idx1]]];
-    };
 
     var pivot =arr[start];
     var swapIdx =start;
@@ -17,19 +18,23 @@ function pivot(arr, start=0, end=arr.length+1){
             swap(arr,swapIdx,i);
         }
     }
+    // console.log(arr)
     swap(arr,start,swapIdx);
     return swapIdx;
 }
 
 function quickSort(arr, left =0, right =arr.length-1){
     if(left <right){
-        let pivotIndex =pivot(arr,left,right) //3
+        let pivotIndex =pivot(arr,left,right)
 //left
         quickSort(arr,left,pivotIndex-1);
 //right
         quickSort(arr,pivotIndex+1,right)
     }
+    console.log(arr)
     return arr;
 }
+var arrVal =[4,8,2,1,5,7,6,3];
+// console.log(pivot([4,8,2,1,5,7,6,3]))
+quickSort(arrVal)
 
-console.log(pivot([4,8,2,1,5,7,6,3]))
